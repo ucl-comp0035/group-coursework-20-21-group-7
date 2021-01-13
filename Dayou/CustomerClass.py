fd5dd153d60f201113da29771dd5c1710f7fd79b
Dayou <zcemdc0@ucl.ac.uk>
2021-01-13 20:39:29 +0800

    Upload Written Classes

---
+++ Dayou/CustomerClass.py
@@ -0,0 +1,33 @@
+import RegisteredUserClass
+
+'''
+Student Name: Dayou Chen
+Purpose: Inherit from the RegisteredUser Class, contains Customer information
+Instance variables: (userID: user ID, password: password, registerDate: 
+registered date, firstName: first name, lastName: last name, email: email, age: age)
+Methods: __init__: Create an customer object(account); updateProfile: Change the
+email or/and the age of the customer; __repr__: display customer info
+'''
+
+
+class Customer(RegisteredUserClass.RegisteredUser):
+    """
+    Inherit from parent RegisterUser Class，
+    follows the Liskov substitution principle
+    """
+
+    def __init__(self, userID: int, password: str, registerDate: str,
+                 firstName: str, lastName: str, email: str, age: int):
+        super().__init__(userID, password, registerDate)
+        self.__firstName = firstName
+        self.__lastName = lastName
+        self.__email = email
+        self.__age = age
+
+    def updateProfile(self, email: str, age: int):
+        self.__email = email
+        self.__age = age
+
+    def __repr__(self):
+        return 'Customer(firstName={},lastName={},email={},age={})' \
+            .format(self.__firstName, self.__lastName, self.__email, self.__age)
---
+++ Dayou/CustomerTest.py
@@ -0,0 +1,24 @@
+import unittest
+import CustomerClass
+
+class MyTestCase(unittest.TestCase):
+    def test_init_Customer(self):
+        customer = CustomerClass.Customer(1000, 'abc', '08012021', 'Dayou',
+                                          'Chen', 'abc@ucl.ac.uk', 21)
+        self.assertEqual(customer.__repr__(),
+                         'Customer(firstName=Dayou,lastName=Chen,email=abc@ucl.ac.uk,age=21)')
+
+    def test_updateProfile(self):
+        customer = CustomerClass.Customer(1000, 'abc', '08012021', 'Dayou',
+                                          'Chen', 'abc@ucl.ac.uk', 21)
+        self.assertEqual(customer.__repr__(),
+                         'Customer(firstName=Dayou,lastName=Chen,email=abc@ucl.ac.uk,age=21)')
+        customer.updateProfile('dayou@ucl.ac.uk',21)
+        self.assertEqual(customer.__repr__(),
+                         'Customer(firstName=Dayou,lastName=Chen,email=dayou@ucl.ac.uk,age=21)')
+        customer.updateProfile('dayou@ucl.ac.uk',22)
+        self.assertEqual(customer.__repr__(),
+                         'Customer(firstName=Dayou,lastName=Chen,email=dayou@ucl.ac.uk,age=22)')
+
+if __name__ == '__main__':
+    unittest.main()
