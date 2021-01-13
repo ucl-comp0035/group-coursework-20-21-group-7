df999ba40a0c209e5b858a3a317ebe973e2583b4
Dayou <zcemdc0@ucl.ac.uk>
2021-01-13 21:25:26 +0800

    Add Author name

--- Dayou/CustomerClass.py
+++ Dayou/CustomerClass.py
@@ -1,6 +1,7 @@
 import RegisteredUserClass
 
 '''
+Student name: Dayou Chen
 Purpose: Inherit from the RegisteredUser Class, contains Customer information
 Instance variables: (userID: user ID, password: password, registerDate: 
 registered date, firstName: first name, lastName: last name, email: email, age: age)
