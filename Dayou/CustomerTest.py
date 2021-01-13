be53abf2c4918fa33dc902cdb3e479f8b9a12fc7
Dayou <zcemdc0@ucl.ac.uk>
2021-01-13 20:48:45 +0800

    Update test

--- Dayou/CustomerTest.py
+++ Dayou/CustomerTest.py
@@ -3,6 +3,7 @@ import CustomerClass
 
 class MyTestCase(unittest.TestCase):
     def test_init_Customer(self):
+        print('Test: init_Customer')
         customer = CustomerClass.Customer(1000, 'abc', '08012021', 'Dayou',
                                           'Chen', 'abc@ucl.ac.uk', 21)
         self.assertEqual(customer.__repr__(),
@@ -13,10 +14,12 @@ class MyTestCase(unittest.TestCase):
                                           'Chen', 'abc@ucl.ac.uk', 21)
         self.assertEqual(customer.__repr__(),
                          'Customer(firstName=Dayou,lastName=Chen,email=abc@ucl.ac.uk,age=21)')
+        print('Test: update age to 21')
         customer.updateProfile('dayou@ucl.ac.uk',21)
         self.assertEqual(customer.__repr__(),
                          'Customer(firstName=Dayou,lastName=Chen,email=dayou@ucl.ac.uk,age=21)')
         customer.updateProfile('dayou@ucl.ac.uk',22)
+        print('Test: update age to 22')
         self.assertEqual(customer.__repr__(),
                          'Customer(firstName=Dayou,lastName=Chen,email=dayou@ucl.ac.uk,age=22)')
 
